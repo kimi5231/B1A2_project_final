ca726ea8c15f929e867bffd72291b22109ef497d
Im caheeun <dev.chaeng@gmail.com>
2025-08-02 01:00:57 +0900

    전체 화면 모드 추가(F9 입력)

--- B1A2_project_final/B1A2_project_final/GameFramework.h
+++ B1A2_project_final/B1A2_project_final/GameFramework.h
@@ -39,6 +39,8 @@ public:
 	void OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
 	LRESULT CALLBACK OnProcessingWindowMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
 
+	// 전체 화면 
+	void ChangeSwapChainState();
 private:
 	HINSTANCE m_hInstance;
 	HWND m_hWnd;
