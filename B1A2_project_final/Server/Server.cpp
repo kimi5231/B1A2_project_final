c57f78a0f168d67cbbfeff7764252f394ecb7b3c
Lim Sooyoung <djwjod@naver.com>
2025-08-31 20:55:10 +0900

    Server Non-Blocking I/O로 코드 수정

--- B1A2_project_final/Server/Server.cpp
+++ B1A2_project_final/Server/Server.cpp
@@ -1,5 +1,7 @@
 ﻿#include "pch.h"
 
+// Socket 클래스 추가? (Socket, RecvBuffer, SendBuffer 포함)
+
 int main()
 {
 	//소켓 프로그래밍을 위한 WinSock 라이브러리 초기화 
@@ -7,12 +9,16 @@ int main()
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-	// client의 접속을 감지할 listen socket 생성
+	// client의 접속을 감지할 listen socket을 비동기로 생성
 	// AF_INET: IPv4
 	// SOCK_STREAM: TCP
-	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET listenSocket = ::WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
 	if (listenSocket == 0)
 		return 0;
+
+	// listenSocket을 Non-Blocking Socket으로 설정
+	u_long mode = 1;
+	ioctlsocket(listenSocket, FIONBIO, &mode);
 
 	SOCKADDR_IN serverAddr;
 	// 주소/포트 정보를 담을 구조체 초기화
@@ -32,29 +38,78 @@ int main()
 	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
 		return 0;
 
+	std::cout << "Server Start" << std::endl;
+
+	int clientNum = 0;
+
+	std::vector<WSAPOLLFD> pollfds;
+	std::vector<SOCKET> clients;
+
 	while (true)
 	{
-		SOCKADDR_IN clientAddr;
-		::memset(&clientAddr, 0, sizeof(clientAddr));
-		int addrLen = sizeof(clientAddr);
+		pollfds.clear();
 
-		// client 접속 시 통신하기 위한 client socket 생성
-		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
-		if (clientSocket == INVALID_SOCKET)
-			return 0;
-
-		std::cout << "Connected" << std::endl;
-
-		while (true)
+		// clientSocket 관찰 대상 등록
+		for (SOCKET client : clients)
 		{
-			char recvBuffer[100];
-			// 수신
-			int recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-			if (recvLen <= 0)
-				return 0;
+			WSAPOLLFD pollfd;
+			pollfd.events = POLLRDNORM;
+			pollfd.fd = client;
+			pollfd.revents = 0;
+			pollfds.push_back(pollfd);
+		}
 
-			std::cout << "Recv Data: " << recvBuffer << std::endl;
-			std::cout << "Recv Data Len: " << recvLen << std::endl;
+		// listenSocket 관찰 대상 등록
+		WSAPOLLFD pollfd;
+		pollfd.events = POLLRDNORM;
+		pollfd.fd = listenSocket;
+		pollfd.revents = 0;
+		pollfds.push_back(pollfd);
+
+		::WSAPoll(pollfds.data(), pollfds.size(), 100);
+
+		for (WSAPOLLFD pollfd : pollfds)
+		{
+			// 이벤트가 발생했을 경우
+			if (pollfd.revents != 0)
+			{
+				// listenSocket
+				if (pollfd.fd == listenSocket)
+				{
+					SOCKADDR_IN clientAddr;
+					::memset(&clientAddr, 0, sizeof(clientAddr));
+					int addrLen = sizeof(clientAddr);
+
+					// client 접속 시 통신하기 위한 client socket 생성
+					SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+					if (clientSocket == INVALID_SOCKET)
+						return 0;
+
+					// clientSocket을 Non-Blocking Socket으로 설정
+					u_long mode = 1;
+					ioctlsocket(clientSocket, FIONBIO, &mode);
+
+					clients.push_back(clientSocket);
+
+					std::cout << "Connected" << std::endl;
+				}
+				else
+				{
+					char recvBuffer[100];
+					// 수신
+					int recvLen = ::recv(pollfd.fd, recvBuffer, sizeof(recvBuffer), 0);
+					if (recvLen <= 0)
+					{
+						closesocket(pollfd.fd);
+						clients.erase(std::remove(clients.begin(), clients.end(), pollfd.fd), clients.end());
+						std::cout << "DisConnected" << std::endl;
+						break;
+					}
+
+					std::cout << "Recv Data: " << recvBuffer << std::endl;
+					std::cout << "Recv Data Len: " << recvLen << std::endl;
+				}
+			}
 		}
 	}
 
